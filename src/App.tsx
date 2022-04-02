f1438ccf75e4f26aa9289fe88ca4f36a4bc97f2a
Rafa prieto <rafaprieto063@gmail.com>
2022-04-02 09:11:32 +0200

    se sube proyecto terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 
 type FormElement = React.FormEvent<HTMLFormElement>
 
@@ -23,19 +23,59 @@ function App(): JSX.Element {
     const newTasks = [...task, { name, done: false }];
     setTask(newTasks);
   }
+  {/*chage state tasks */ }
+
+  const changeButtonTasks = (i: number): void => {
+    const newTasks: Itask[] = [...task];
+    newTasks[i].done = !newTasks[i].done
+    setTask(newTasks)
+  }
+
+  { /*Delete task */ }
+  const deleteTask = (i: number): void => {
+    const newTasks: Itask[] = [...task];
+    newTasks.splice(i, 1);
+    setTask(newTasks);
+  }
+
+
+
+
+
 
   return (
-    <Fragment>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} value={newTask} />
-        <button type="submit">Add task</button>
-      </form>
-      {
-        task.map((t: Itask, i: number) => {
-          return <h1 key={i}>{t.name}</h1>
-        })
-      }
-    </Fragment>
+    <div className='container'>
+      <div className='col s4 m6'>
+        <div className='card blue-grey darken-1'>
+          <div className='card-content white-text'>
+            <form onSubmit={handleSubmit} className='col s12'>
+              <input
+                className='input-task'
+                type="text" onChange={(e) => setNewTask(e.target.value)}
+                value={newTask}
+                autoFocus
+              />
+              <button type="submit" className='btn waves-effect waves-light'>Add task</button>
+            </form>
+            {task.map((t: Itask, i: number) => (
+              <div className='container col s4' key={i}>
+                <h5 style={{ textDecoration: t.done ? 'line-through' : '' }}>{t.name}</h5>
+                <button className='btn-floating btn-large waves-effect waves-light red' onClick={() => changeButtonTasks(i)}>
+                  {t.done ? '✓' : '✗'}
+                </button>
+                <button className='btn-floating btn-large waves-effect waves-light red' onClick={() => deleteTask(i)}>
+                  🗑
+                </button>
+              </div>
+            ))}
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 export default App;
